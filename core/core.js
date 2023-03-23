32ee40162e28a60a709326d40b9b3aa722f1bb68
MohsenEMX <zmc.site@gmail.com>
2023-03-23 23:31:33 +0430

    Web Terminal Update 3

--- core/core.js
+++ core/core.js
@@ -15,6 +15,7 @@ inp.addEventListener("keydown", function(e) {
         execute(e);
     }
 });
+
 function execute(e) {
     // All code goes here
     cmd_content = inp.value;
@@ -22,24 +23,55 @@ function execute(e) {
     cmd_parts[0] = cmd_parts[0].toLowerCase();
     var isEcho = cmd_parts[0].indexOf("echo");
     var isSCL = cmd_parts[0].indexOf("scl");
-    if (isEcho == 0) echo(cmd_parts);
+    var isTitle = cmd_parts[0].indexOf("title");
+    var isClear = cmd_parts[0].indexOf("clear");
+    var isClear2 = cmd_parts[0].indexOf("cls");
+    var isColor = cmd_parts[0].indexOf("color");
+    if (isEcho == 0) echo(cmd_content);
     else if (isSCL == 0) scl();
+    else if (isTitle == 0) title(cmd_content);
+    else if (isClear == 0 || isClear2 == 0) clear();
+    else if (isColor == 0) color(cmd_content);
     else failed();
     inp.value = "";
     out.innerHTML += "<br />";
     inp.style.width = 250 + "px";
 }
 function echo(content) {
-    let fullSen = content.slice(5,5000);
-    console.log(fullSen);
-    out.innerHTML += fullSen;
+    let fullSen = content.slice(5);
+    if (fullSen.trim() === '') {
+        out.innerHTML += "Echo is on";
+    } else {
+        out.innerHTML += fullSen;
+    }
 }
 function title(content) {
-
+    let fullSen = content.slice(6);
+    if (fullSen.trim() === '') {
+        out.innerHTML += "Current page title is: " + "<span class=\"special\"> " + document.title + " </span>";
+    } else {
+        document.title = fullSen;
+        out.innerHTML += "Page title is now: " + "<span class=\"special\"> " + document.title + " </span>";
+    }
 }
 function failed() {
     out.innerHTML += "Error: Entered command is not valid";
 }
 function scl() {
     popup.style.display = 'flex';
+}
+function clear() {
+    out.innerHTML = "";
+}
+function color(color) {
+    let ccolor = color.slice(6);
+    let eccolor = ccolor.slice(0,1);
+    let f;
+    if (eccolor.indexOf("#") == 0) {
+        out.style.color = ccolor;
+    }
+    // if (ccolor == 1) f = "#999999";
+}
+function exit(code) {
+    let ecode = code.slice(5);
 }
